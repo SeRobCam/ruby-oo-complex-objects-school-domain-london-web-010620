629e17d5f6fc74dd2fc1b08ec037ae2c83bb2f10
Sergio Robles <sroblesc@gmail.com>
2020-01-09 15:22:33 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+
+class School 
+  
+  attr_accessor :student, :grade, :name
+  
+  def initialize(name)
+    @name = name 
+    @grade = grade
+    @student = student
+  end
+  
+  def roster 
+    roster = {}
+  end
+  
+  def add_student(grade, name)
+    roster[:grade] = []
+    roster[:grade] << name
+  end
+  
+  def grade 
+    
+  end
+  
+  def sort(student)
+    roster.student.sort
+  end
+  
+end 
